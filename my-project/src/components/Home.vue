0d3b7534e2a47a6b8e57a16e765164c1a0f492fe
Ilia Kurbakov <kurbakovila@gmail.com>
2018-12-27 01:51:42 +0300

    go

---
+++ my-project/src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+
+  <div>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+            <v-carousel-item
+              v-for="ad in ads"
+              :key="ad.id"
+              :src="ad.imageSrc"
+            >
+              <div class="car-link">
+                <v-btn class="error" :to="'/ad/' + ad.id">{{ad.title}}</v-btn>
+              </div>
+            </v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex
+          v-for="ad of ads"
+          :key="ad.id"
+          xs12
+          sm6
+          md4
+        >
+          <v-card>
+            <v-img
+              :src="ad.imageSrc"
+              aspect-ratio="2.75"
+              height="200px"
+            ></v-img>
+
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+            </v-card-title>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+              <v-btn raised class="primary">Buy</v-btn>
+            </v-card-actions>
+
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    data() {
+      return {
+        ads: [
+          {
+            title: 'First',
+            description: 'ssdsdsdsdsdsds',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            id: '123'
+          },
+          {
+            title: 'Firsts',
+            description: 'ssdsdsdsdssdfdsds',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            id: '1234'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .car-link {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    background: rgba(0, 0, 0, .5);
+    transform: translate(-50%, 0);
+    padding: 5px 15px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+  }
+</style>
